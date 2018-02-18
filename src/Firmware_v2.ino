55079d017adc20a80ae5d564edb614c7e97d17f8
Sam Hall <osusam28@gmail.com>
2018-02-18 14:30:47 -0500

    added basic pubsub heartbeat data

--- src/Firmware_v2.ino
+++ src/Firmware_v2.ino
@@ -1,18 +1,38 @@
 /*
  * Project Firmware_v2
  * Description:
- * Author:
- * Date:
+ * Author: Sam Hall
+ * Date: 2018-02-18
  */
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
+ int onboardLED = D7;
+ int count = 0;
 
-}
+ void setup() {
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+   pinMode(onboardLED, OUTPUT);
 
-}
+ }
+
+ void loop() {
+   // To blink the LED, first we'll turn it on...
+   digitalWrite(onboardLED, HIGH);
+
+   delay(500);
+
+   // Then we'll turn it off...
+   digitalWrite(onboardLED, LOW);
+
+   delay(500);
+
+   if(count > 60) {
+     // Get some data
+     String data = "Message from photon! ;)";
+     // Trigger the integration
+     Particle.publish("weighme-heartbeat", data, PRIVATE);
+
+     count = 0;
+   }
+
+   count++;
+ }
